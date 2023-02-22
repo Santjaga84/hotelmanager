1175f63036c795199d9f9843da7d92960739cca4
Oleksii <Santjaga84@gmail.com>
2023-02-22 21:20:48 +0200

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Router } from 'react-router-dom';
+import { Switch } from 'antd';
+import App from './pages/App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux';
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+    <Provider store={store}>
     <App />
+    </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
